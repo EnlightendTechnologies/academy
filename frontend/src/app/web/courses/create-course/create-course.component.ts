27fa9bbbb2ce45f2dc5e47e2bc241f0015fcffc0
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2023-09-13 21:56:06 +0500

    M

--- frontend/src/app/web/courses/create-course/create-course.component.ts
+++ frontend/src/app/web/courses/create-course/create-course.component.ts
@@ -13,11 +13,12 @@ import { CourseService } from 'src/app/shared/service/course.service';
   styleUrls: ['./create-course.component.scss']
 })
 export class CreateCourseComponent {
-  isisLoading:boolean = false;
-  courseForm:FormGroup;
-  
+  isLoading: boolean = false;
+  courseForm: FormGroup;
+  uploadedImages: string[] = [];
 
-  constructor(private formBuilder: FormBuilder,private courseService:CourseService) { 
+
+  constructor(private formBuilder: FormBuilder, private courseService: CourseService) {
     this.courseForm = this.formBuilder.group({
       id: [''],
       name: [''],
@@ -26,16 +27,39 @@ export class CreateCourseComponent {
       imageUrl: [''],
     });
   }
-  submitForm():void {
+  submitForm(): void {
     console.log(this.courseForm);
-    if(this.courseForm.invalid) return;
+    if (this.courseForm.invalid) return;
     this.courseService.createCourse(this.courseForm.value).subscribe({
-      next:(response) => {
-        this.isisLoading = false;
+      next: (response) => {
+        this.isLoading = false;
         this.courseForm.reset();
       },
       error: (error) => console.error('Failed to fetch courses:', error),
     })
-     
+
+  }
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.saveImageToLocalstorage(file);
+    }
+  }
+
+  saveImageToLocalstorage(file: File) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const imageData = reader.result as string;
+
+      // Store the image data in localStorage (not recommended for large images).
+      localStorage.setItem('uploadedImage', imageData);
+
+      // Optionally, you can display the image on the page.
+      const imageElement = new Image();
+      imageElement.src = imageData;
+      document.body.appendChild(imageElement);
+    };
+
+    reader.readAsDataURL(file);
   }
 }
